e6483cbc3c30179bc49140211928a627d6338950
Alex <rutol@mail.ru>
2016-08-01 00:07:22 +0300

    Поиск файлов версия 2.

--- src/test/java/ru/stqa/selenium/SearchMovie.java
+++ src/test/java/ru/stqa/selenium/SearchMovie.java
@@ -2,11 +2,9 @@ package ru.stqa.selenium;
 
 import org.testng.annotations.*;
 import org.openqa.selenium.*;
-import java.io.PrintWriter;
-import java.io.FileOutputStream;
-import java.io.FileNotFoundException;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 
 public class SearchMovie extends TestNgTestBase{
@@ -15,18 +13,7 @@ public class SearchMovie extends TestNgTestBase{
   @Test
   public void testSearchMovie() throws Exception {
 
-	  int count_exist=0, count_notexist=0;
-// creating report file  
-    PrintWriter repfile = null;
-         try
-         {
-            repfile = new PrintWriter(new FileOutputStream("Search_rep.txt"));
-         }
-         catch(FileNotFoundException e)
-         {
-             System.out.println("Ошибка открытия файла Search_rep.txt");
-             System.exit(0);
-         }    
+	//  int count_exist=0, count_notexist=0;
 
 	  
 	// Login block    
@@ -39,9 +26,32 @@ public class SearchMovie extends TestNgTestBase{
 	    
 	driver.findElement(By.linkText("Home")).click();  /* Go to home page */
 	
+	//Add movie block    
+    driver.findElement(By.cssSelector("img[alt=\"Add movie\"]")).click();
+    driver.findElement(By.name("imdbid")).clear();
+    driver.findElement(By.name("imdbid")).sendKeys("6");
+    driver.findElement(By.name("name")).clear();
+    driver.findElement(By.name("name")).sendKeys("Abra-Kadabra");
+    driver.findElement(By.name("year")).clear();
+    driver.findElement(By.name("year")).sendKeys("1971");
+    driver.findElement(By.id("text_languages_0")).clear();
+    driver.findElement(By.id("text_languages_0")).sendKeys("Russian");
+    driver.findElement(By.name("country")).clear();
+    driver.findElement(By.name("country")).sendKeys("USSR");
+    driver.findElement(By.id("submit")).click();
+	
+    driver.findElement(By.linkText("Home")).click();  /* Go to home page */
+
+ // Select lists to show all movies on one page      
+    WebElement menu1=driver.findElement(By.id("category"));
+    WebElement menu2=driver.findElement(By.id("n"));
+
+    new Select(menu1).selectByVisibleText("All categories");
+    new Select(menu2).selectByVisibleText("All results per page");
+
 	//Search existing movie
 	driver.findElement(By.id("q")).clear();
-    driver.findElement(By.id("q")).sendKeys("best"); //
+    driver.findElement(By.id("q")).sendKeys("Abra"); 
     driver.findElement(By.id("q")).sendKeys(Keys.RETURN);
 
     // Waiting results
@@ -49,9 +59,7 @@ public class SearchMovie extends TestNgTestBase{
     wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("results")));    
     Assert.assertTrue(driver.findElement(By.id("results")).findElements(By.tagName("a")).size() > 0);
     
-    count_exist = driver.findElements(By.xpath("//a[contains(@href,'go=movie')]")).size();
-
-	//Search not existing movie
+    //Search not existing movie
 	driver.findElement(By.id("q")).clear();
     driver.findElement(By.id("q")).sendKeys("ddddd");
     driver.findElement(By.id("q")).sendKeys(Keys.RETURN);
@@ -61,22 +69,6 @@ public class SearchMovie extends TestNgTestBase{
     wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("results")));    
     Assert.assertTrue(driver.findElement(By.id("results")).findElements(By.tagName("a")).size() == 0);
     
-    count_notexist = driver.findElements(By.xpath("//a[contains(@href,'go=movie')]")).size();
-
-    //Check counts and print result into report file
-    if ((count_exist>0)&&(count_notexist==0)) {
-    	repfile.println("Correct search movie Test\n");
-    	repfile.println(count_exist+"\n");
-    	repfile.println(count_notexist+"\n");
-    	repfile.close();
-    	}
-    else { 
-    	repfile.println("Incorrect search movie \n");
-    	repfile.println(count_exist+"\n");
-    	repfile.println(count_notexist+"\n");
-    	repfile.close();
-    	}
-
   }
 
 }
